02904f11147490f5ad32567fa5889c3bfff4abdb
oguzhan2027 <oguzhancelik2027@gmail.com>
2023-07-11 16:02:27 +0300

    first commit

--- WebApplication2/Controllers/StatisticController.cs
+++ WebApplication2/Controllers/StatisticController.cs
@@ -10,10 +10,16 @@ namespace WebApplication2.Controllers
     public class StatisticController : Controller
     {
         casgemEntities2 db = new casgemEntities2();
+
+        
+
         public ActionResult Index()
         {
             ViewBag.employeeCount = db.TblEmployee.Count();
             ViewBag.maxSalaryEmployee = db.TblEmployee.Where(x => x.EmployeeSalary == salary).Select(y => y.EmployeeName + "" + y.EmployeeSurname).FirstOrDefault();
+            ViewBag.totalCityCount=db.TblEmployee.Distinct().Count();
+            ViewBag.avgEmployeeSalary= db.TblEmployee.Average(x=> x.EmployeeSalary);
+            ViewBag.countSofWareDepartment=db.TblEmployee.Where(x=>x.EmployeeDepartment==db.TblDepartment.Where(z=>z.DepartmentName=="Yazılım").Select(y=>y.DepartmentID).FirstOrDefault()).Count();
             return View();
         }
     }
